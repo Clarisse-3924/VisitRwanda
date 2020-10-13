443e1265d3cd17ab15e5eb02c37d76a040752e42
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-10-13 18:12:12 +0200

    modifying html file,javascript,css

---
+++ scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $(".hike").click(function(){
+        $("#hotel").show();
+    });
+    $("#hotel").click(function(){
+        $("#hotel").hide();
+        
+    });
+    $("#hotels").click(function(){
+        $("#hotel").show();
+    });
+    $(".hotel").click(function(){
+        $("#hotel").hide();
+    });
+    $(".Culture").click(function(){
+        $("#hotel").show();
+        
+    });
+    $("#hotel").click(function(){
+        $(".Culture").show();
+        
+    });
+
+    $(".history").click(function(){
+        $("#hotel").show();
+        
+    });
+    $("#hotel").click(function(){
+        $(".history").show();
+        
+    });
+  
+  
+  });
