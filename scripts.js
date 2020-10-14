57c066929f50865409ce682ca0c81b12afc61cca
Clarisse-3924 <cuwizeyimana3924@gmail.com>
2020-10-14 13:26:34 +0200

    modifying files

--- scripts.js
+++ scripts.js
@@ -1,39 +1,19 @@
+$(document).ready(function() {
+    $("button#green").click(function() {
+      $("body").removeClass();
+      $("body").addClass("green-background");
+    });
 
+    $("button#yellow").click(function() {
+      $("body").removeClass();
+      $("body").addClass("yellow-background");
+    });
 
- $(document).ready(function(){
-        $(".hike").click(function(){
-            $(".hike").toggle();
-            $("#hotel").toggle();
-        });
-       
-        });
-        $("#hotels").click(function(){
-            $("#hotels").toggle();
-            $(".hotel").toggle();
-        });
-        $(".hotel").click(function(){
-            $("#hotels").toggle();
-            $(".hotel").toggle();
-        });
-        $(".Culture").click(function(){
-            $(".Culture").toggle();
-            $("#hotel").toggle();
-        });
-        $("#hotel").click(function(){
-            $(".Culture").toggle();
-            $("#hotel").toggle();
-        });
-
-        $(".history").click(function(){
-            $(".history").toggle();
-            $("#hotel").toggle();
-        });
-        $("#hotel").click(function(){
-            $(".history").toggle();
-            $("#hotel").toggle();
-        });
-
-
-  
-  
+    $("button#red").click(function() {
+      $("body").removeClass();
+      $("body").addClass("red-background");
+    });
+  });
+  $("button#green").click(function() {
+    $("body").css("background-color", "green");
   });
