4ad6225d57e25127570c10686428de306823042e
umedith <umedith03@gmail.com>
2020-10-15 21:11:08 +0200

    add the js file

---
+++ aboutcss/aboutjs/aboutus.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.doing').click(function () {
+         $('.content1').show()
+    })   
+});
+$(document).ready(function () {
+    $('.mission').click(function () {
+        //  $('.content1').hide()
+         $('.content2').show()
+    })   
+});
